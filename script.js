4517908c5dc3f6231baff0092199987aa755e0e4
Dogan2200 <dqan00@gmail.com>
2025-07-09 14:14:40 +0300

    Update script.js
    Signed-off-by: Dogan2200 <dqan00@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Karşılaştırma sistemi yüklendi.');
+function openPopup() {
+  document.getElementById('popup').classList.remove('hidden');
+}
+function closePopup() {
+  document.getElementById('popup').classList.add('hidden');
+}
+function showTab(id) {
+  document.querySelectorAll('.tab').forEach(t => t.classList.add('hidden'));
+  document.getElementById(id).classList.remove('hidden');
+}
