b0477d3533bf38ae48c7248cba42eeb5858c8f3e
Dogan2200 <dqan00@gmail.com>
2025-07-09 16:28:22 +0300

    Update script.js
    Signed-off-by: Dogan2200 <dqan00@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function openPopup() {
   document.getElementById('popup').classList.remove('hidden');
 }
+
 function closePopup() {
   document.getElementById('popup').classList.add('hidden');
 }
+
 function showTab(id) {
   document.querySelectorAll('.tab').forEach(t => t.classList.add('hidden'));
   document.getElementById(id).classList.remove('hidden');
